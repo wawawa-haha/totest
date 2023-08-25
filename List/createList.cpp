aa65d2c089911e9cc7f0f0cff5b4ea53a2708746
wawawa-haha <59327080+wawawa-haha@users.noreply.github.com>
2023-08-25 14:04:48 +0800

    Create createList.cpp

---
+++ List/createList.cpp
@@ -0,0 +1,42 @@
+class MyLinkedList {
+public:
+
+·struct LinkedNode {
+        int val;
+        LinkedNode* next;
+        LinkedNode(int val):val(val), next(nullptr){}
+    };
+    MyLinkedList() {
+
+    }
+    
+    int get(int index) {
+
+    }
+    
+    void addAtHead(int val) {
+
+    }
+    
+    void addAtTail(int val) {
+
+    }
+    
+    void addAtIndex(int index, int val) {
+
+    }
+    
+    void deleteAtIndex(int index) {
+
+    }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
